416b879d96615c7fe70a6419edf13f9894d3aa04
AshuGit003 <116780167+AshuGit003@users.noreply.github.com>
2026-01-09 23:31:40 +0530

    Made an one root setup for application (#7)
    * chore: remove angular cache from repository
    
    * Make a one root setup for application
    
    ---------
    
    Co-authored-by: Ashishgawade <ashish.gawade@eilisys.com>

--- backend/GroupFundTracker.Api/Program.cs
+++ backend/GroupFundTracker.Api/Program.cs
@@ -1,5 +1,6 @@
 using GroupFundTracker.Api.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,23 +13,43 @@ builder.Services.AddDbContext<GroupFundDbContext>(options =>
         builder.Configuration.GetConnectionString("DefaultConnection")
     ));
 
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAngular",
-        policy => policy
-        .WithOrigins("http://localhost:4200")
-        .AllowAnyHeader()
-        .AllowAnyMethod());
-});
+//builder.Services.AddCors(options =>
+//{
+//    options.AddPolicy("AllowAngular",
+//        policy => policy
+//        .WithOrigins("http://localhost:4200")
+//        .AllowAnyHeader()
+//        .AllowAnyMethod());
+//});
 
 var app = builder.Build();
 
 app.UseHttpsRedirection();
 
-app.UseCors("AllowAngular");
+// Serve index.html automatically at "/"
+app.UseDefaultFiles(new DefaultFilesOptions
+{
+    FileProvider = new PhysicalFileProvider(
+        Path.Combine(builder.Environment.ContentRootPath, "wwwroot", "browser")),
+    RequestPath = ""
+});
 
+
+// Serve Angular from wwwroot/browser
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+        Path.Combine(builder.Environment.ContentRootPath, "wwwroot", "browser")),
+    RequestPath = ""
+});
+
+//app.UseCors("AllowAngular");
+
+app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapFallbackToFile("index.html");
 
 app.Run();
