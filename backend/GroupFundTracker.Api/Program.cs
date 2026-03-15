d924213b9fa46c4da258c4317502a3e95253a0a3
AshuGit003 <116780167+AshuGit003@users.noreply.github.com>
2026-03-15 23:57:05 +0530

    Update Program.cs

--- backend/GroupFundTracker.Api/Program.cs
+++ backend/GroupFundTracker.Api/Program.cs
@@ -50,6 +50,12 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<GroupFundDbContext>();
+    db.Database.Migrate();
+}
+
 app.MapFallbackToFile("index.html");
 
 app.Run();
